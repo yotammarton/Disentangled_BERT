827bee698c466f3fe72631c434306fa96f3459cd
yotammarton <yonam92@gmail.com>
2021-12-03 12:55:08 +0200

    Training for achieving working EncoderDecoderModel

--- dataset.py
+++ dataset.py
@@ -6,7 +6,12 @@ from transformers import BertTokenizer
 def _process_data_to_model_inputs(batch, tokenizer):
     # https://colab.research.google.com/drive/1WIk2bxglElfZewOHboPFNj8H44_VAyKE?usp=sharing
     # tokenize the texts
-    tokenized = tokenizer(batch["story"], padding="max_length", truncation=True)
+    tokenized = tokenizer(batch["story"],
+                          padding="max_length",
+                          truncation=True,
+                          # original max length is 512
+                          # max tokens in datasets for one sentence, reducing cuda memory usage
+                          max_length=74)
 
     batch["input_ids"] = tokenized.input_ids
     batch["attention_mask"] = tokenized.attention_mask
@@ -23,9 +28,9 @@ def _process_data_to_model_inputs(batch, tokenizer):
 
 
 def load_preprocess_glucose_dataset(batch_size, tokenizer):
-    train_data = load_dataset("glucose", script_version="0.0.0", split="train[5%:]")
-    val_data = load_dataset("glucose", script_version="0.0.0", split="train[:5%]")
-    test_data = load_dataset("glucose", script_version="0.0.0", split="test")
+    train_data = load_dataset("glucose", split="train[5%:]")
+    val_data = load_dataset("glucose", split="train[:5%]")
+    test_data = load_dataset("glucose", split="test")
 
     process_data_to_model_inputs = partial(_process_data_to_model_inputs, tokenizer=tokenizer)
 
@@ -44,6 +49,65 @@ def load_preprocess_glucose_dataset(batch_size, tokenizer):
     return {'train': map_data(train_data), 'val': map_data(val_data), 'test': map_data(test_data)}
 
 
+def load_preprocess_cnn_dataset(batch_size):
+    import datasets
+    train_data = datasets.load_dataset("cnn_dailymail", "3.0.0", split="train")
+    from transformers import BertTokenizerFast
+    tokenizer = BertTokenizerFast.from_pretrained("bert-base-uncased")
+
+    encoder_max_length = 512
+    decoder_max_length = 128
+
+    def process_data_to_model_inputs(batch):
+        # tokenize the inputs and labels
+        inputs = tokenizer(batch["article"], padding="max_length", truncation=True, max_length=encoder_max_length)
+        outputs = tokenizer(batch["highlights"], padding="max_length", truncation=True, max_length=decoder_max_length)
+
+        batch["input_ids"] = inputs.input_ids
+        batch["attention_mask"] = inputs.attention_mask
+        batch["decoder_input_ids"] = outputs.input_ids
+        batch["decoder_attention_mask"] = outputs.attention_mask
+        batch["labels"] = outputs.input_ids.copy()
+
+        # because BERT automatically shifts the labels, the labels correspond exactly to `decoder_input_ids`.
+        # We have to make sure that the PAD token is ignored
+        batch["labels"] = [[-100 if token == tokenizer.pad_token_id else token for token in labels] for labels in
+                           batch["labels"]]
+
+        return batch
+
+    train_data = train_data.select(range(128))
+
+    train_data = train_data.map(
+        process_data_to_model_inputs,
+        batched=True,
+        batch_size=batch_size,
+        remove_columns=["article", "highlights", "id"]
+    )
+
+    train_data.set_format(
+        type="torch", columns=["input_ids", "attention_mask", "decoder_input_ids", "decoder_attention_mask", "labels"],
+    )
+
+    val_data = datasets.load_dataset("cnn_dailymail", "3.0.0", split="validation[:10%]")
+
+    val_data = val_data.select(range(32))
+
+    val_data = val_data.map(
+        process_data_to_model_inputs,
+        batched=True,
+        batch_size=batch_size,
+        remove_columns=["article", "highlights", "id"]
+    )
+
+    val_data.set_format(
+        type="torch", columns=["input_ids", "attention_mask", "decoder_input_ids", "decoder_attention_mask", "labels"],
+    )
+
+    return {'train': train_data, 'val': val_data}
+
+
 if __name__ == "__main__":
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    data = load_preprocess_glucose_dataset(batch_size=32, tokenizer=tokenizer)
+    # data = load_preprocess_glucose_dataset(batch_size=32, tokenizer=tokenizer) # TODO
+    cnn_data = load_preprocess_cnn_dataset(batch_size=8)  # TODO
--- train.py
+++ train.py
@@ -1,5 +1,10 @@
-import sys
 import os
+
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "1,2,3"  # TODO modify if needed
+
+import wandb
+import sys
 import json
 import torch
 from functools import partial
@@ -8,10 +13,12 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 from transformers import BertTokenizerFast
-from model import EncoderDecoderModel  # A local copy of the source code with our modifications
+# from model import EncoderDecoderModel  # A local copy of the source code with our modifications # TODO uncomment
 from transformers import BertTokenizer, BertModel, BertForMaskedLM, BertConfig, BertLMHeadModel, PretrainedConfig, \
-    Seq2SeqTrainer, Seq2SeqTrainingArguments
-from dataset import load_preprocess_glucose_dataset
+    Seq2SeqTrainer, Seq2SeqTrainingArguments, EncoderDecoderModel  # TODO remove original EncoderDecoderModel
+from dataset import load_preprocess_glucose_dataset, load_preprocess_cnn_dataset  # TODO cnn del
+
+os.environ["WANDB_DISABLED"] = "true"  # TODO wandb
 
 
 def create_encoder_decoder_model(split_embedding=False):
@@ -34,7 +41,7 @@ def create_encoder_decoder_model(split_embedding=False):
         num_hidden_layers=12,
         pad_token_id=0,
         position_embedding_type="absolute",
-        transformers_version="4.4.2",
+        transformers_version="4.13.0.dev0",
         type_vocab_size=2,
         use_cache=True,
         vocab_size=30522)
@@ -60,7 +67,7 @@ def create_encoder_decoder_model(split_embedding=False):
         num_hidden_layers=12,
         pad_token_id=0,
         position_embedding_type="absolute",
-        transformers_version="4.4.2",
+        transformers_version="4.13.0.dev0",
         type_vocab_size=2,
         use_cache=True,
         vocab_size=30522,
@@ -68,18 +75,21 @@ def create_encoder_decoder_model(split_embedding=False):
         add_cross_attention=True)
     decoder = BertLMHeadModel(config=decoder_config)
 
-    model = EncoderDecoderModel(encoder=encoder, decoder=decoder, split_embedding=split_embedding)
-
+    model = EncoderDecoderModel(encoder=encoder,
+                                decoder=decoder,
+                                # split_embedding=split_embedding  # TODO only in our model from `model.py`
+                                )
     return model
 
 
 def train_model(model):
     # Train the model for one epoch
-    # TODO
+    # TODO Implement
     model.train()
 
 
 def eval_model(model):
+    # TODO Implement
     model.eval()
 
 
@@ -101,7 +111,28 @@ def _compute_metrics(pred, rouge, tokenizer):
 
 
 def run():
+    """
+    os.environ["CUDA_VISIBLE_DEVICES"] = "2,3"
+    batch_size = 96
+    per_device_train_batch_size=48
+    ml-srv3              Mon Nov 15 00:57:27 2021  460.67
+    [0] Tesla M40        | 26'C,   0 % |     0 / 11448 MB |
+    [1] Tesla M40        | 25'C,   0 % |     0 / 11448 MB |
+    [2] Tesla M40        | 62'C,  99 % | 11372 / 11448 MB | yotamm:python/659(11367M)
+    [3] Tesla M40        | 64'C,  98 % |  8922 / 11448 MB | yotamm:python/659(8917M)
+
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3"
     batch_size = 32
+    per_device_train_batch_size=32
+    ml-srv3              Wed Nov 17 17:45:32 2021  460.67
+    [0] Tesla M40        | 52'C,  68 % |  9474 / 11448 MB | yotamm:python/2101(9469M)
+    [1] Tesla M40        | 52'C,  84 % |  6560 / 11448 MB | yotamm:python/2101(6555M)
+    [2] Tesla M40        | 50'C,  90 % |  6560 / 11448 MB | yotamm:python/2101(6555M)
+    [3] Tesla M40        | 52'C, 100 % |  6560 / 11448 MB | yotamm:python/2101(6555M)
+    """
+    n_gpus = len(os.environ["CUDA_VISIBLE_DEVICES"].split(","))
+    batch_size = 8
+    print(f'[*] Using GPU(s): {os.environ["CUDA_VISIBLE_DEVICES"]} (n_gpus: {n_gpus}) and batch_size: {batch_size}')
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
     data = load_preprocess_glucose_dataset(batch_size=batch_size, tokenizer=tokenizer)
@@ -115,17 +146,20 @@ def run():
 
     # noinspection PyTypeChecker
     training_args = Seq2SeqTrainingArguments(
-        predict_with_generate=True,
+        predict_with_generate=True,  # TODO buggy
+        # generation_max_length=128, # TODO needed?
         per_device_train_batch_size=batch_size,
         per_device_eval_batch_size=batch_size,
-        # fp16=True, TODO CUDA Only
+        fp16=True,  # TODO CUDA Only https://github.com/ThilinaRajapakse/simpletransformers/issues/646
+        fp16_full_eval=True,  # TODO needed?
         output_dir="./",
-        num_train_epochs=1,  # TODO just for checking everything works
+        num_train_epochs=3,  # TODO just for checking everything works
         # save_steps=10,  # TODO if we use save_strategy="steps"
         evaluation_strategy="epoch",
         logging_strategy="epoch",
         save_strategy="epoch",
         # warmup_steps=2000, # TODO linear increasing of the lr
+        gradient_accumulation_steps=1  # TODO 1 is the default
     )
 
     # TODO metric can be changed
@@ -139,16 +173,16 @@ def run():
         args=training_args,
         compute_metrics=compute_metrics,
         train_dataset=train_data,
-        eval_dataset=val_data
+        eval_dataset=val_data,
     )
 
     trainer.train()
 
 
 if __name__ == "__main__":
+    wandb.init(project="disentangled_bert", entity="yotammartin")  # TODO remove in the end
     run()
     exit(777)
-
 """
 #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*
 Some extra code 
@@ -161,7 +195,7 @@ model = create_encoder_decoder_model(split_embedding=False)
 
 """Verify we got a random / pretrained weights compared to a pretrained model"""
 # # initialize Bert2Bert from pre-trained checkpoints
-# org_model = EncoderDecoderModel.from_encoder_decoder_pretrained('bert-base-uncased', 'bert-base-uncased')
+org_model = EncoderDecoderModel.from_encoder_decoder_pretrained('bert-base-uncased', 'bert-base-uncased')
 #
 # # look at the parameters weights. `org_model` is the pretrained ones. compare to desired `model`.
 # encoder_params_my = list(model.encoder.parameters())
